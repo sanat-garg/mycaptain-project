b9a7644bdef519d412cc478440d6fdb2625cc66e
sanat-garg <87259729+sanat-garg@users.noreply.github.com>
2021-07-11 13:54:18 +0530

    Create project1.py
    Project 1

---
+++ project1.py
@@ -0,0 +1,5 @@
+from math import pi
+radius = int(input('\n\nInput the radius of the circle: '))
+area = pi*(radius*radius)
+print("The area of the circle with radius "+str(radius)+" is: "+str(format(area,".2f")))
+print('\n\n')
