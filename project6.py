ca9812852cf4f9cc3f6ab1cd794b4ab22e6045db
Sanat Garg <87259729+sanat-garg@users.noreply.github.com>
2021-08-26 19:16:21 +0530

    Update project6.py

--- project6.py
+++ project6.py
@@ -1,5 +1,5 @@
-e = {0, 2, 4, 6, 8}
-n = {1, 2, 3, 4, 5}
+e = {0, 2, 4, 6, 8, 12}
+n = {1, 4, 7, 9, 11, 12}
 print("Union of E and N =",e | n)
 print("Intersection of E and N =",e & n)
 print("Difference of E and N =",e - n)
