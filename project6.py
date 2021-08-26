9d68f8fc6aef816764bfd9438bd579860b3212f4
Sanat Garg <87259729+sanat-garg@users.noreply.github.com>
2021-08-26 19:14:39 +0530

    Create project6.py

---
+++ project6.py
@@ -0,0 +1,6 @@
+e = {0, 2, 4, 6, 8}
+n = {1, 2, 3, 4, 5}
+print("Union of E and N =",e | n)
+print("Intersection of E and N =",e & n)
+print("Difference of E and N =",e - n)
+print("Symmetric difference of E and N =",e ^ n)
